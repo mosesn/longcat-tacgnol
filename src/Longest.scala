7307e276e7560edd15d76f46e99df14e76b054f7
Moses Nakamura <nnythm@gmail.com>
2013-03-06 03:27:46 -0500

    beauty

--- src/Longest.scala
+++ src/Longest.scala
@@ -1,37 +1,47 @@
 import scala.annotation.tailrec
 
 object Longest {
-  case class Candidate(lower: Int, upper: Int, contents: String) {
+  case class Candidate(lower: Int, upper: Int, chars: List[Char]) {
     val (smaller, bigger) = (lower - 1, upper + 1)
+
     def next(string: String): Option[Candidate] =
       if (smaller >= 0 && bigger <= string.length - 1 && string(smaller) == string(bigger)) {
-        Some(copy(lower = lower - 1, upper = upper + 1, contents = string.substring(smaller, bigger + 1)))
+        Some(copy(lower = smaller, upper = bigger, chars = string(smaller) :: chars))
       }
       else {
         None
       }
+
+    def stringify: String = "%s%s".format(chars.mkString, chars.init.reverse.mkString)
   }
 
   def main(args: Array[String]) = {
     val string = args(0)
-    val sideLength = (string.length + 1 / 2) + 1
     val candidates = (string.zipWithIndex map {
-      case (char, index) => Candidate(index, index, char.toString)
+      case (char, index) => Candidate(index, index, char :: Nil)
     }).toList
-    println(search(string, candidates, candidates(0).contents))
+    println(search(string, candidates, candidates.head))
+    println(parSearch(string, candidates))
   }
 
   @tailrec
-  def search(string: String, candidates: List[Candidate], best: String): String = {
+  def search(string: String, candidates: List[Candidate], best: Candidate): String = {
     candidates match {
-      case Nil => best
-      case head  :: tail => {
-        val next = head.next(string)
-        next match {
-          case Some(candidate) => search(string, tail :+ candidate, candidate.contents)
-          case None => search(string, tail, best)
-        }
+      case Nil => best.stringify
+      case head :: tail => head.next(string) match {
+        case Some(candidate) => search(string, tail :+ candidate, candidate)
+        case None => search(string, tail, best)
       }
     }
   }
+
+  def parSearch(string: String, candidates: List[Candidate]): String = {
+    @tailrec
+    def consecutiveSearch(candidate: Candidate): String = candidate.next(string) match {
+      case Some(next) => consecutiveSearch(next)
+      case None => candidate.stringify
+    }
+    candidates.par.map(consecutiveSearch(_)).maxBy(_.length)
+  }
+
 }
